9eeb98c38b63e3c8f5a10a1dd24030154e54db0c
Sagar Parajuli <official.sagar@yahoo.com>
2020-04-14 21:46:50 +1000

    home

---
+++ app/src/main/java/com/world/news/international/VerticalSpace.java
@@ -0,0 +1,26 @@
+package com.world.news.international;
+
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Created by rishabh on 01-03-2016.
+ */
+public class VerticalSpace extends RecyclerView.ItemDecoration {
+    int Space;
+    public VerticalSpace(int Space){
+        this.Space=Space;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        outRect.left=Space;
+        outRect.bottom=Space;
+        outRect.right=Space;
+        if (parent.getChildLayoutPosition(view)==0){
+            outRect.top=Space;
+        }
+    }
+}
